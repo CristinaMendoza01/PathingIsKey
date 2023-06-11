87a32079a00b4b7baa6bfe221cd5d9061078339c
javiech1 <92743651+javiech1@users.noreply.github.com>
2023-06-11 20:55:22 +0200

    add:prevent platform from generating when obstacle

--- 3DAssets_Deliverable/Assets/Scripts/EmptyObjectGenerator.cs
+++ 3DAssets_Deliverable/Assets/Scripts/EmptyObjectGenerator.cs
@@ -16,56 +16,50 @@ public class EmptyObjectGenerator : MonoBehaviour
         GeneratePlatforms();
     }
 
-    public void GeneratePlatforms(){
-        GameObject waterRiver = GameObject.FindGameObjectWithTag("WaterRiver");
-        GameObject lavaRiver = GameObject.FindGameObjectWithTag("LavaRiver");
-        for (int i = 0; i < numberOfObjectsX; i++)
+   public void GeneratePlatforms()
+{
+    GameObject waterRiver = GameObject.FindGameObjectWithTag("WaterRiver");
+    GameObject lavaRiver = GameObject.FindGameObjectWithTag("LavaRiver");
+
+    for (int i = 0; i < numberOfObjectsX; i++)
+    {
+        for (int j = 0; j < numberOfObjectsZ; j++)
         {
-            for (int j = 0; j < numberOfObjectsZ; j++)
+            // Position of the next object
+            Vector3 position = new Vector3((i+1) * separation, 0, (j+1) * separation);
+
+            // Get the size of the platform prefab
+            Vector3 platformSize = Platform.GetComponent<BoxCollider>().size;
+
+            // Calculate the bounds of the platform
+            Bounds platformBounds = new Bounds(position, platformSize);
+
+            // Check for overlapping colliders with the "EmptyPlatform" or "Obstacle" tag
+            Collider[] overlappingColliders = new Collider[10]; // Adjust the size as per your needs
+            int numOverlappingColliders = Physics.OverlapBoxNonAlloc(platformBounds.center, platformSize / 2, overlappingColliders);
+
+            bool overlapWithEmptyOrObstacle = false;
+
+            // Check each overlapping collider and if in the position there is no object with the "EmptyPlatform" or "Obstacle" tag, then generate a new object
+            for (int r = 0; r < numOverlappingColliders; r++)
             {
-                // Position of the next object
-                Vector3 position = new Vector3((i+1) * separation, 0, (j+1) * separation);
-                
-                // Generates a new object with the Platform prefab
+                Collider collider = overlappingColliders[r];
+                if (collider.CompareTag("Empty") || collider.CompareTag("Obstacle"))
+                {
+                    overlapWithEmptyOrObstacle = true;
+                    break;
+                }
+            }
+
+            if (!overlapWithEmptyOrObstacle)
+            {
+                // Generates a new platform
                 GameObject newObj = Instantiate(Platform);
                 newObj.transform.position = position;
-
-                // SI LO HA HECHO UNA VEZ, MIRA QUE NO SE SOBREPONGA CON OTRA EMPTYPLATFORM CREADA ANTERIORMENTE
-                // SI SE SOBREPONE NO DEBERIA CREARSE
-                if (Done)
-                {
-                    // Get the size of the platform
-                    Vector3 platformSize = newObj.GetComponent<BoxCollider>().size;
-
-                    // Calculate the bounds of the platform
-                    Bounds platformBounds = new Bounds(newObj.transform.position, platformSize);
-
-                    // Check for overlapping colliders with the "EmptyPlatform" tag
-                    Collider[] overlappingColliders = new Collider[10]; // Adjust the size as per your needs
-                    int numOverlappingColliders = Physics.OverlapBoxNonAlloc(platformBounds.center, platformSize / 2, overlappingColliders);
-
-                    bool overlapWithEmpty = false;
-
-                    // Check each overlapping collider
-                    for (int r = 0; r < numOverlappingColliders; r++)
-                    {
-                        Collider collider = overlappingColliders[r];
-                        if (collider.CompareTag("Empty"))
-                        {
-                            overlapWithEmpty = true;
-                            break;
-                        }
-                    }
-
-                    if (overlapWithEmpty)
-                    {
-                        Destroy(newObj); // Destroy the newly instantiated platform
-                    }
-                }
-
-
             }
         }
-        Done = true;
     }
+    Done = true;
+}
+
 }
